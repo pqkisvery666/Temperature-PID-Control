5829fba7498dd2d6973718dcf5173a8435e46aa1
pqkisvery666 <133090386+pqkisvery666@users.noreply.github.com>
2025-02-09 18:45:50 +0800

    Add files via upload

---
+++ System/Timer1.c
@@ -0,0 +1,41 @@
+#include "stm32f10x.h"                  // Device header
+
+void Timer4_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+	
+	TIM_InternalClockConfig(TIM1);
+	
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInitStructure.TIM_Period = 10000 - 1;
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 7200 - 1;
+	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
+	
+	TIM_ClearFlag(TIM1, TIM_FLAG_Update);
+	TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	TIM_Cmd(TIM4, ENABLE);
+}
+
+/*
+void TIM2_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	{
+		
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+	}
+}
+*/
---
+++ System/Timer1.h
@@ -0,0 +1,6 @@
+#ifndef __TIMER4_H
+#define __TIMER4_H
+
+void Timer4_Init(void);
+
+#endif
